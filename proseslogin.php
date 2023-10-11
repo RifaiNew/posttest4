c9389b1433b4e915dbbe05cf2666dfa6e9ec386b
RifaiNew <66177993+RifaiNew@users.noreply.github.com>
2023-10-11 21:29:06 +0800

    Add files via upload

---
+++ proseslogin.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+if(isset($_POST["usernamebaru"]) && isset($_POST["passwordbaru"])  ){
+	$_SESSION['usernamebaru'] = $_POST["usernamebaru"];
+	$_SESSION['passwordbaru'] = $_POST["passwordbaru"];
+}
+
+if($_SESSION['usernamebaru'] == $_SESSION['username'] && $_SESSION['passwordbaru'] == $_SESSION['password']){?>
+	<script type="text/javascript">
+		window.alert("Berhasil Login");
+		window.location.assign("index.php");
+	</script>
+<?php 
+}
+
+else{?>
+	<script type="text/javascript">
+		window.alert("Gagal Login");
+		window.location.assign("login.php");
+	</script>
+<?php 
+} 
+?>
+
+
